0e3bc561d2c14f0a474cb7c7d12a842d96d4064a
Kieran Klukas <92754843+kcoderhtml@users.noreply.github.com>
2025-02-14 04:09:17 -0500

    feat: add led support

--- firmware/main.py
+++ firmware/main.py
@@ -1,5 +1,6 @@
 # You import all the IOs of your board
 import board
+import random
 
 # These are imports from the kmk library
 from kmk.kmk_keyboard import KMKKeyboard
@@ -7,6 +8,8 @@ from kmk.scanners.keypad import KeysScanner
 from kmk.keys import KC
 from kmk.modules.macros import Press, Release, Tap, Macros
 from kmk.extensions.media_keys import MediaKeys
+from kmk.extensions.RGB import RGB
+from kmk.handlers.sequences import simple_key_sequence
 
 # This is the main instance of your keyboard
 keyboard = KMKKeyboard()
@@ -15,6 +18,23 @@ keyboard = KMKKeyboard()
 macros = Macros()
 keyboard.modules.append(macros)
 keyboard.extensions.append(MediaKeys())
+rgb = RGB(pixel_pin=board.GP6, num_pixels=6)
+keyboard.extensions.append(rgb)
+
+# Define RGB patterns
+def random_blink():
+    pattern = []
+    for i in range(6):
+        pattern.append((255, 191, 0)) # Amber color
+    rgb.animation_mode = 'static'
+    rgb.pixels = pattern
+    rgb.brightness = random.random()
+    rgb.refresh()
+
+@keyboard.before_matrix_scan
+def key_rgb_handler(keyboard):
+    if keyboard.matrix.find_pressed_keys():
+        random_blink()
 
 # Define your pins here!
 PINS = [board.D11, board.D2, board.D9, board.D1, board.D8, board.D10, board.D3, board.D4, board.D6]
